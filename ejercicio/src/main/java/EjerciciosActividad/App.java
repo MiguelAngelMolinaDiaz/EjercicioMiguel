d9854ea8d7c7e2f604a34bfd02c476084e35ebbd
Miguel Angel Molina Diaz <kamigz772@gmail.com>
2026-04-25 02:27:35 +0000

    Uptade activity new files

---
+++ ejercicio/src/main/java/EjerciciosActividad/App.java
@@ -0,0 +1,14 @@
+package EjerciciosActividad;
+
+public class App {
+    public static void main(String[] args) {
+
+        Avion avion = new Avion();
+        Gafas gafas = new Gafas();
+        Pan pan = new Pan();
+        Persona persona = new Persona();
+        Arbol arbol = new Arbol();
+        Tamal tamal = new Tamal();
+
+    }
+}
---
+++ ejercicio/src/main/java/EjerciciosActividad/Tamal.java
@@ -0,0 +1,52 @@
+package EjerciciosActividad;
+
+public class Tamal {
+    private String color;
+    private String tamaño;
+    private String region;
+    private float precio;
+    private float peso;
+
+    public Tamal() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
