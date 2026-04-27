02ea182ee434b22c83cfa933929b5e40f9cc1459
Miguel Angel Molina Diaz <kamigz772@gmail.com>
2026-04-27 20:04:15 +0000

    Uptade file

--- ejercicio/src/main/java/EjerciciosActividad/App.java
+++ ejercicio/src/main/java/EjerciciosActividad/App.java
@@ -3,12 +3,44 @@ package EjerciciosActividad;
 public class App {
     public static void main(String[] args) {
 
+        // Instanciación de objetos (<<instantiate>>)
         Avion avion = new Avion();
         Gafas gafas = new Gafas();
         Pan pan = new Pan();
+        Tamal tamal = new Tamal();
         Persona persona = new Persona();
         Arbol arbol = new Arbol();
-        Tamal tamal = new Tamal();
 
+        // Asignación de valores según tus atributos reales
+        avion.setColor("Blanco");
+        avion.setAltura(10.5f);
+        avion.setEdad(5);
+        avion.setDiametro(3.2f);
+        avion.setTipo("Comercial");
+
+        gafas.setMarca("Ray-Ban");
+        gafas.setColor("Negro");
+
+        pan.setTipo("Integral");
+        pan.setPrecio(2500.0f);
+
+        tamal.setRegion("Tolima");
+        tamal.setPrecio(5000.0f);
+
+        persona.setColorOjos("Cafes");
+        persona.setDedos(10);
+
+        arbol.setTipo("Pino");
+        arbol.setAltura(6.5f);
+
+        // Salida por consola
+        System.out.println("Objetos creados correctamente:");
+        System.out.println("Avion tipo: " + avion.getTipo());
+        System.out.println("Avion altura: " + avion.getAltura());
+        System.out.println("Gafas marca: " + gafas.getMarca());
+        System.out.println("Pan tipo: " + pan.getTipo());
+        System.out.println("Tamal region: " + tamal.getRegion());
+        System.out.println("Persona ojos: " + persona.getColorOjos());
+        System.out.println("Arbol tipo: " + arbol.getTipo());
     }
 }
